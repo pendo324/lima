66e6266124e8feaa495b82c74c8580b6e3c9e0f1
Justin Alvarez <alvajus@amazon.com>
2023-06-23 18:50:14 -0400

    [WIP] feat(WSL2): fix containerd service path
    Signed-off-by: Justin Alvarez <alvajus@amazon.com>

--- pkg/cidata/cidata.TEMPLATE.d/boot/40-install-containerd.sh
+++ pkg/cidata/cidata.TEMPLATE.d/boot/40-install-containerd.sh
@@ -57,7 +57,7 @@ EOF
 EOF
 
 	# Add NetworkNamespacePath=/var/run/netns/lima-wsl to the end of the [Unit] section of the containerd service
-	sed -i'' '/^\[Unit\]/!b;:a;n;/./ba;iNetworkNamespacePath=/var/run/netns/lima-wsl' /lib/systemd/system/containerd.service
+	sed -i'' '/^\[Unit\]/!b;:a;n;/./ba;iNetworkNamespacePath=/var/run/netns/lima-wsl' /usr/local/lib/systemd/system/containerd.service
 
 	systemctl enable --now containerd buildkit stargz-snapshotter
 fi
