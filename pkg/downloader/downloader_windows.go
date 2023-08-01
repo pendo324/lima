0b10de3cb398b869222b2bfdea4b2e448e3d45ad
Justin Alvarez <alvajus@amazon.com>
2023-08-01 18:32:47 -0400

    [WIP] feat(WSL2): fix decompressor logic
    Signed-off-by: Justin Alvarez <alvajus@amazon.com>

--- pkg/downloader/downloader_windows.go
+++ pkg/downloader/downloader_windows.go
@@ -4,5 +4,19 @@
 package downloader
 
 func Decompressor(ext string) ([]string, bool) {
-	return []string{"7z", "x"}, true
+	var program string
+	switch ext {
+	case ".gz":
+		program = "7z"
+	case ".bz2":
+		program = "7z"
+	case ".xz":
+		program = "7z"
+	case ".zst":
+		program = "7z"
+	default:
+		return nil, false
+	}
+
+	return []string{program, "x"}, true
 }
