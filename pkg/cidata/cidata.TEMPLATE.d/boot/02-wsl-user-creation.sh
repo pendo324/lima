871d8479cd4ff543b45413ddc451d2fc206d1d00
Justin Alvarez <alvajus@amazon.com>
2023-08-02 13:22:35 -0400

    [WIP] feat(WSL2): fix create user logic syntax
    Signed-off-by: Justin Alvarez <alvajus@amazon.com>

--- pkg/cidata/cidata.TEMPLATE.d/boot/02-wsl-user-creation.sh
+++ pkg/cidata/cidata.TEMPLATE.d/boot/02-wsl-user-creation.sh
@@ -3,7 +3,7 @@
 # when using a WSL2 VM.
 [ "$LIMA_VMTYPE" = "wsl" ] || exit 0
 
-sudo useradd -u "${$LIMA_CIDATA_UID}" "${$LIMA_CIDATA_USER}" -d /home/"${$LIMA_CIDATA_USER}".linux/
-sudo mkdir /home/"${$LIMA_CIDATA_USER}".linux/.ssh/
-sudo cp "${LIMA_CIDATA_MNT}"/ssh_authorized_keys /home/"${$LIMA_CIDATA_USER}".linux/.ssh/authorized_keys
-sudo chown "${$LIMA_CIDATA_USER}" /home/"${$LIMA_CIDATA_USER}".linux/.ssh/authorized_keys
+sudo useradd -u "${LIMA_CIDATA_UID}" "${LIMA_CIDATA_USER}" -d /home/"${LIMA_CIDATA_USER}".linux/
+sudo mkdir /home/"${LIMA_CIDATA_USER}".linux/.ssh/
+sudo cp "${LIMA_CIDATA_MNT}"/ssh_authorized_keys /home/"${LIMA_CIDATA_USER}".linux/.ssh/authorized_keys
+sudo chown "${LIMA_CIDATA_USER}" /home/"${LIMA_CIDATA_USER}".linux/.ssh/authorized_keys
