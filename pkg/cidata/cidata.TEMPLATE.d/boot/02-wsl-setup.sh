3f8de677c16b4093bc556528fa8ad50742103b2e
Justin Alvarez <alvajus@amazon.com>
2023-08-02 16:12:50 -0400

    [WIP] feat(WSL2): fix requirement check
    Signed-off-by: Justin Alvarez <alvajus@amazon.com>

--- pkg/cidata/cidata.TEMPLATE.d/boot/02-wsl-setup.sh
+++ pkg/cidata/cidata.TEMPLATE.d/boot/02-wsl-setup.sh
@@ -7,3 +7,7 @@ sudo useradd -u "${LIMA_CIDATA_UID}" "${LIMA_CIDATA_USER}" -d /home/"${LIMA_CIDA
 sudo mkdir /home/"${LIMA_CIDATA_USER}".linux/.ssh/
 sudo cp "${LIMA_CIDATA_MNT}"/ssh_authorized_keys /home/"${LIMA_CIDATA_USER}".linux/.ssh/authorized_keys
 sudo chown "${LIMA_CIDATA_USER}" /home/"${LIMA_CIDATA_USER}".linux/.ssh/authorized_keys
+
+# copy some CIDATA to the hardcoded path for requirement checks (TODO: make this not hardcoded)
+sudo mkdir -p /mnt/lima-cidata
+sudo cp "${LIMA_CIDATA_MNT}"/meta-data /mnt/lima-cidata/meta-data
