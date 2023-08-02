6d4e31dd96ac6c7aab0fa0891796af24b8a6e284
Justin Alvarez <alvajus@amazon.com>
2023-08-02 17:08:03 -0400

    [WIP] feat(WSL2): update sudoers
    Signed-off-by: Justin Alvarez <alvajus@amazon.com>

--- pkg/cidata/cidata.TEMPLATE.d/boot/02-wsl-setup.sh
+++ pkg/cidata/cidata.TEMPLATE.d/boot/02-wsl-setup.sh
@@ -3,10 +3,14 @@
 # when using a WSL2 VM.
 [ "$LIMA_VMTYPE" = "wsl" ] || exit 0
 
+# create user
 sudo useradd -u "${LIMA_CIDATA_UID}" "${LIMA_CIDATA_USER}" -d /home/"${LIMA_CIDATA_USER}".linux/
 sudo mkdir /home/"${LIMA_CIDATA_USER}".linux/.ssh/
 sudo cp "${LIMA_CIDATA_MNT}"/ssh_authorized_keys /home/"${LIMA_CIDATA_USER}".linux/.ssh/authorized_keys
 sudo chown "${LIMA_CIDATA_USER}" /home/"${LIMA_CIDATA_USER}".linux/.ssh/authorized_keys
+
+# add lima to sudoers
+sudo echo "lima ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/99_lima_sudoers
 
 # copy some CIDATA to the hardcoded path for requirement checks (TODO: make this not hardcoded)
 sudo mkdir -p /mnt/lima-cidata
